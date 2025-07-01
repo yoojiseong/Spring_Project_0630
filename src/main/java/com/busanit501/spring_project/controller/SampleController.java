dcfc0ed6b14cf24dd52ed714c245191ff62fe1fd
yoojiseong <yujiseong588@gmail.com>
2025-07-01 12:36:05 +0900

    register파일 생성

--- src/main/java/com/busanit501/spring_project/controller/SampleController.java
+++ src/main/java/com/busanit501/spring_project/controller/SampleController.java
@@ -3,6 +3,9 @@ package com.busanit501.spring_project.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @Log4j2
@@ -12,4 +15,30 @@ public class SampleController {
     public void hello() {
         log.info("hello.......");
     }
+
+    //단순 파라미터 자동수집
+    @GetMapping("ex1")
+    // 기본 자료형으로 정의
+    // 화면에서 쿼리스트링 형식(get방식)으로 데이터를 전달
+    // http://localhost:8080/ex1?name=ujs&age=20
+    public void ex1(String name, int age) {
+        log.info("SampleController에서 작업중.");
+        log.info("name: " + name+", age: " + age);
+    }
+
+    @GetMapping("/ex2")
+    public void ex2(@RequestParam(name = "name", defaultValue = "사용자") String name,
+                    @RequestParam(name = "age", defaultValue = "24") int age){
+        log.info("SampleController에서 작업중. ex2, 값이 없는 경우 기본 값 이용");
+        log.info("name: " + name+", age: " + age);
+    }
+
+    // 날짜 포맷터 인 경우
+    // http://localhost:8080/ex3?dueDate=2025-07-01
+    @GetMapping("/ex3")
+    public void ex3(LocalDate dueDate){
+        log.info("SampleController에서 작업중. ex3, 날짜 포맷터 형식인 경우");
+        log.info("dueDate: " + dueDate);
+    }
+
 }
--- src/main/java/com/busanit501/spring_project/controller/TodoController.java
+++ src/main/java/com/busanit501/spring_project/controller/TodoController.java
@@ -2,7 +2,9 @@ package com.busanit501.spring_project.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Log4j2
@@ -15,9 +17,26 @@ public class TodoController {
     //최종 url : /todo/list
     @RequestMapping("/list")
     //void라고 하면 메서드명 : list
-    // /WEB-INF/views/list.jsp를 카리킹
+    // /WEB-INF/views/todo/list.jsp를 카리킹
     // 자동 연결
     public void list(){
         log.info("TodoController에서 작업중, list 호출");
+    }
+    // 최종 url : /todo/register
+    // 메소드 : post
+    // void 라고하면, 메서드 명 : register
+    // /WEB-INF/views/todo/register.jsp , 가리킴.
+    // 자동 연결, 뷰 리졸버라는 친구의 업무.
+    @RequestMapping(value = "/register", method= RequestMethod.GET)
+    public void register(){
+        log.info("TodoController에서 작업, register호출");
+    }
+
+    //로직 처리
+    //최종 url : /todo/register
+    // 메소드 : post
+    @PostMapping("/register")
+    public void registerPost(){
+        log.info("TodoController에서 작업, register(post) 로직처리");
     }
 }
